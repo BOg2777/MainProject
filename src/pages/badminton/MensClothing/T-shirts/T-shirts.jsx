0e5ee531f9451fdeba8f7333d696098cd12f50c7
BOg <timohamaluh.2005@mail.ru>
2022-11-27 01:51:06 +0300

    bugs fixed

--- src/pages/badminton/MensClothing/T-shirts/T-shirts.jsx
+++ src/pages/badminton/MensClothing/T-shirts/T-shirts.jsx
@@ -7,13 +7,13 @@ import logo2 from '../../../../img/Mens-T-shirt/T-shirt_KAWASAKIST-T1026BLUE.png
 
 function TShirts() {
     console.log(logo);
-    let listPoructT_shirts = [
+    let listProductT_shirts = [
         {img: logo, description: 'Поло мужское LI-NING', model: 'APLH217-1', price: '950 ₽'},
         {img: logo2, description: 'Мужская футболка KAWASAKI', model: 'ST-T1026 BLUE', price: '1 600 ₽'}];
 
     return (
         <div className={styles.wrapper}>
-            {listPoructT_shirts.map((item) => {
+            {listProductT_shirts.map((item) => {
                 return <TShirt item={item} />
             })}
         </div>
