79f23d661c62cc5f9ffd7cbe5358626d4705764c
ZhenyaVardomskiy <zhenya.vardomskiy99@gmail.com>
2022-11-29 18:07:45 +0300

    error correction

--- src/components/NewsItem/NewsItem.jsx
+++ src/components/NewsItem/NewsItem.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 
-import style from './style.module.css';
+import styles from './style.module.css';
 
 
 function NewsItem({ item }) {
+
   return (
-    <div className={style.wrapperItem}>
-        <div className={style.title}>{item.title}</div>
-        <div className={style.wrapperText}>
-          <div className={style.text}>{item.text}</div>
-          <div className={style.text, style.data}>{item.data}</div>
+    <>
+      <div className={styles.wrapper}>
+        <div className={styles.wrapperItem}>
+          <div className={styles.title}>{item.title}</div>
+          <div className={styles.wrapperText}>
+            <div className={styles.text}>{item.text}</div>
+            <div className={`${styles.text} ${styles.data}`}>{item.data}</div>
+          </div>
         </div>
-    </div>
+      </div>
+      <div className={styles.imgWrapper}>
+        <img src={item.img} />
+      </div>
+    </>
   );
 }
 
--- src/pages/news/News.jsx
+++ src/pages/news/News.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 
-import style from './style.module.css';
+import styles from './style.module.css';
 import NewsItem from "../../components/NewsItem/NewsItem";
+import item1 from "../../img/News/podacha.jpg" 
+
 
 
 function News() {
 
     let newsList = [
-        {title: 'Ночная лига. Играй в бадминтон ночью!' , text: 'Любительская ассоциация бадминтона (ЛАБ) совместно с магазином бадминтонной экипировки запустили серию турниров "Ночная лига".' , data: '05.07.2022'},
+        {title: 'Ночная лига. Играй в бадминтон ночью!' , text: 'Любительская ассоциация бадминтона (ЛАБ) совместно с магазином бадминтонной экипировки запустили серию турниров "Ночная лига".' , data: '05.07.2022' , img: item1 },
         {title: 'Ракетки и экипировка KAWASAKI теперь представлены в Линин-центр' , text: 'Мотоциклы, детали для Boeing и Embaer, промышленные роботы и … ракетки для бадминтона. Казалось бы, что может объединить эти вещи. Технологии!' , data: '04.07.2022'},
         {title: 'Ракетки и экипировка KAWASAKI теперь представлены в Линин-центр' , text: 'Мотоциклы, детали для Boeing и Embaer, промышленные роботы и … ракетки для бадминтона. Казалось бы, что может объединить эти вещи. Технологии!' , data: '04.07.2022'}];
 
     return (
         <div>
-            <div className={style.title}>Новости</div>
-            <div className={style.filter}>
-                <input className={style.input} type="text" placeholder="Поиск" />
-                <a className={style.filterCategory}>Сортировать по: Последние добавленные</a>
+            <div className={styles.title}>Новости</div>
+            <div className={styles.filter}>
+                <input className={styles.input} type="text" placeholder="Поиск" />
+                <a className={styles.filterCategory}>Сортировать по: Последние добавленные</a>
             </div>
-            <div className={style.listitems}>
+            <div className={styles.listItems}>
             {newsList.map((item) => {
                 return <NewsItem item={item} />
             })}
