aa6659004f86dc688800d17222522b94eae6163c
ZhenyaVardomskiy <zhenya.vardomskiy99@gmail.com>
2022-12-01 00:22:52 +0300

    added burger menu and page links

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom'
 
 import "./App.css";
 import Header from './components/Header/Header';
@@ -7,11 +8,15 @@ import News from './pages/news/News';
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
-      <TShirts />
-      < News />
-    </div>
+        <div className='wrapper'>
+          <Routes>
+            <Route path='/Stock' element={ < TShirts /> } />
+            <Route path='/News' element={ < News /> }/>     
+          </Routes>
+        </div>
+    </>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 import styles from './styles.module.css';
 
+import NavBar from "../NavBar/NavBar";
 import logo from '../../img/Header/Logo.svg'
 import search from '../../img/Header/Search.svg'
 import {ReactComponent as Menu} from '../../img/Header/Filter.svg'
@@ -10,25 +13,37 @@ import {ReactComponent as User} from '../../img/Header/User.svg'
 import {ReactComponent as ShoppingBag} from '../../img/Header/ShoppingBag.svg'
 
 
+
 function Header() {
+
+  const [nav, setNav] = useState(false);
+  console.log(nav);
+
   return (
- 
-<header className={styles.header}>
-    <nav className={styles.appNavigation}>
-      <ul className={styles.nav}> 
-          <li className={styles.navList}> <Menu className={styles.menu}/> Каталог</li>
-          <li className={styles.navList}>Акции</li>
-          <li className={styles.navList}>Новости</li>
-      </ul>
-    <img src={logo} className={styles.logo}></img> 
-      <ul className={styles.userBar}>
-          <li className={styles.headerSearch}> <img src={search} alt="search"/> <input type="text" placeholder="Поиск" className={styles.search}/></li>
-          <li className={styles.favourites}><Favourite className={styles.favourite}/> </li>
-          <li className={styles.user}><User className={styles.userImg}/></li>
-          <li className={styles.shoppingBag}><ShoppingBag className={styles.shoppingBagImg}/></li>
-    </ul>
-  </nav>
-</header>
+        <>
+          
+          <header className={styles.header}>
+              <nav className={styles.appNavigation}>
+                <ul className={styles.nav}> 
+                    <li className={styles.navList} onClick={() => setNav(!nav)}>
+                    <Menu className={styles.menu}/> Каталог
+                    
+                    </li>
+                    <Link to="/Stock" className={styles.navList}>Акции</Link>
+                    <Link to="/News" className={styles.navList}>Новости</Link>
+                </ul>
+                <Link to="/Main"><img src={logo} className={styles.logo}></img></Link> 
+                <ul className={styles.userBar}>
+                    <li className={styles.headerSearch}> <img src={search} alt="search"/> <input type="text" placeholder="Поиск" className={styles.search}/></li>
+                    <li className={styles.favourites}><Favourite className={styles.favourite}/> </li>
+                    <li className={styles.user}><User className={styles.userImg}/></li>
+                    <li className={styles.shoppingBag}><ShoppingBag className={styles.shoppingBagImg}/></li>
+              </ul>
+            </nav>
+          </header>
+          {nav ? <NavBar/> : ""}
+          
+        </>
   );
 }
 
