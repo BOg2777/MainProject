b5d6be7d551977c027b3ae8bc8ffad533b593487
Polina Fresher <polina_3@icloud.com>
2022-11-30 22:32:16 +0300

    added contacts which is above header

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import "./App.css";
 import Header from './components/Header/Header';
 import TShirts from './pages/badminton/MensClothing/T-shirts/T-shirts';
 import News from './pages/news/News';
+import AboveHeader from './components/AboveHeader/AboveHeader';
 
 function App() {
   return (
     <div>
+      <AboveHeader />
       <Header />
       <TShirts />
       < News />
---
+++ src/components/AboveHeader/AboveHeader.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import styles from "./styles.module.css";
+
+import whatsapp from "../../img/Header/Whatsapp.svg";
+import telegram from "../../img/Header/Telegram.svg";
+
+function AboveHeader() {
+  return (
+    <div className={styles.contacts}>
+      <div className={styles.emailAndAddress}>
+        <span>info@lining.center</span>
+        <span> Адрес: г. Москва, пр-т Буденного, д. 53, к. 2</span>
+      </div>
+      <div className={styles.numberAndMessengers}>
+        <span>МСК: +7 (495) 979-61-31</span>
+        <span className={styles.whatsapp}><img src={whatsapp} alt="whatsapp"/></span>
+        <span className={styles.telegram}><img src={telegram} alt="telegram"/></span>
+      </div>
+    </div>
+  );
+}
+
+export default AboveHeader;
