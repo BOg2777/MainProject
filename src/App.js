e21413107e2d72ddc4fb57c1f312dc6bfadad94e
ZhenyaVardomskiy <zhenya.vardomskiy99@gmail.com>
2022-12-01 03:15:27 +0300

    some fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom'
 
 import "./App.css";
 import Header from './components/Header/Header';
@@ -10,14 +11,18 @@ import Footer from './components/Footer/Footer'
 
 function App() {
   return (
-    <div>
+    <>
       <AboveHeader />
       <Header />
-      <TShirts />
-      < News />
+        <div className='wrapper'>
+          <Routes>
+            <Route path='/Stock' element={ < TShirts /> } />
+            <Route path='/News' element={ < News /> }/>     
+          </Routes>
+        </div>
       <Footer/>
-    </div>
-  );
+    </>
+    );
 }
 
 export default App;
