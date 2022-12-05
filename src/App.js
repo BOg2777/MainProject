d1d627adb02edac86db78fc72d1a7f80467bbd43
Polina Fresher <polina_3@icloud.com>
2022-12-05 17:13:38 +0300

    add-nav-to-categories-component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { useState } from "react";
 
-import { Header,AboveHeader,Footer, Registration,InputUser, InfoAndOffers } from "components";
+import { Header,AboveHeader,Footer, Registration,InputUser, InfoAndOffers,NavToCategories } from "components";
 import { TShirts,News } from "pages";
 
 import 'App.css';
@@ -12,15 +12,17 @@ function App() {
   const [inputShow, setInputShow] = useState(false);
   return (
     <div>
-        <Registration registration={registration} setRegistration={setRegistration} inputShow={inputShow} setInputShow={setInputShow}/>
-        <InputUser registration={registration} setRegistration={setRegistration} inputShow={inputShow} setInputShow={setInputShow} />
-        <AboveHeader />
-        <Header setRegistration={setRegistration} />
-        <Routes>
-              <Route path="/catalog/badminton/men's_clothing/T-shirts" element={ < TShirts /> } />
-              <Route path='/News' element={ < News /> }/>     
-            </Routes>
-        <Footer/>
+      <Registration registration={registration} setRegistration={setRegistration} inputShow={inputShow} setInputShow={setInputShow}/>
+      <InputUser registration={registration} setRegistration={setRegistration} inputShow={inputShow} setInputShow={setInputShow} />
+      <AboveHeader />
+      <Header setRegistration={setRegistration} />
+      <InfoAndOffers />
+      < NavToCategories />
+      <Routes>
+            <Route path='/Stock' element={ < TShirts /> } />
+            <Route path='/News' element={ < News /> }/>     
+          </Routes>
+      <Footer/>
     </div>
   );
 }
