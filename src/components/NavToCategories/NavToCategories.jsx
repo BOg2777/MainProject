6ae711080a09f9926cbe825c181a4218cda75ab2
Polina Fresher <polina_3@icloud.com>
2022-12-07 23:24:51 +0300

    fix-nav-to-categories

--- src/components/NavToCategories/NavToCategories.jsx
+++ src/components/NavToCategories/NavToCategories.jsx
@@ -1,72 +1,37 @@
-import React, { useState } from 'react';
+import React from "react";
 
-import styles from 'components/NavToCategories/styles.module.css';
+import styles from "./styles.module.css";
+
+import { SportCategory } from "components";
+
+const SPORT_CATEGORY = [
+  { title: "Бадминтон", className: "badminton" },
+  { title: "Теннис", className: "tennis" },
+  { title: "Фитнес", className: "fitness" },
+  { title: "Плавание", className: "swimming" },
+  { title: "Скандинавская ходьба", className: "nordicWalking" },
+  { title: "Медицина", className: "medicine" },
+];
 
 function NavToCategories() {
-
-  const [isActive, setActive] = useState({});
-  
-  const switchOff = () => {
-    return setActive({})
-    } 
-
   return (
     <div className={styles.categoriesNav}>
       <div className={styles.descriptionWrapper}>
         <h6 className={styles.headline}>Товары для бадминтона</h6>
         <p className={styles.description}>
-          В LI-NING Сenter представлен исключительно оригинальный и <br/> 
-          качественный товар, который мы заранее отсортировали по <br/>
+          В LI-NING Сenter представлен исключительно оригинальный и <br />
+          качественный товар, который мы заранее отсортировали по <br />
           категориям для удобства поиска.
         </p>
       </div>
 
-    <div className={styles.navCards}>
-
-      <div  className={styles.badminton} onMouseOver={ () => setActive({badminton : true})} onMouseOut={switchOff}>
-        <div className={styles.categories}>
-          <span className={styles.category}> Бадминтон </span>
-          <button className={isActive.badminton ? styles.btn : styles.button}>Смотреть каталог</button>
-        </div>
+      <div className={styles.navCards}>
+        {SPORT_CATEGORY.map((item) => {
+          return (
+            <SportCategory title={item.title} className={item.className} />
+          );
+        })}
       </div>
-
-      <div className={styles.tennis} onMouseOver={ ()=> setActive({tennis : true})} onMouseOut={switchOff}>
-        <div className={styles.categories}>
-          <span className={styles.category}>Теннис</span>
-          <button className={isActive.tennis ? styles.btn : styles.button}>Смотреть каталог</button>
-        </div>
-      </div>
-
-      <div className={styles.fitness} onMouseOver={ ()=> setActive({fitness : true})} onMouseOut={switchOff}>
-        <div className={styles.categories}>
-          <span className={styles.category}>Фитнес</span>
-          <button  className={isActive.fitness ? styles.btn : styles.button}>Смотреть каталог</button>
-        </div>
-      </div>
-
-      <div className={styles.swimming} onMouseOver={ ()=> setActive({swimming : true})} onMouseOut={switchOff}>
-        <div className={styles.categories}>
-          <span className={styles.category}>Плавание</span>
-          <button className={isActive.swimming ? styles.btn : styles.button}>Смотреть каталог</button>
-        </div>
-      </div>
-
-      <div className={styles.nordicWalking} onMouseOver={ ()=> setActive({nordicWalking : true})} onMouseOut={switchOff}>
-        <div className={styles.categories}>
-          <span className={styles.category}>Скандинавская ходьба</span>
-          <button className={isActive.nordicWalking ? styles.btn : styles.button}>Смотреть каталог</button>
-        </div>
-      </div>
-
-      <div className={styles.medicine} onMouseOver={ ()=> setActive({medicine : true})} onMouseOut={switchOff}>
-        <div className={styles.categories}>
-          <span className={styles.category}>Медицина</span>
-          <button  className={isActive.medicine ? styles.btn : styles.button}>Смотреть каталог</button>
-        </div>
-      </div>
-
-    </div>
-
     </div>
   );
 }
