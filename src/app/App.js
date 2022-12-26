f4d734ff1bd2c9bc1681961575e5bd4f5ba505ab
RomaZherko21 <RomaZherko21@inbox.ru>
2022-12-26 20:31:31 +0300

    edits

--- src/app/App.js
+++ src/app/App.js
@@ -1,38 +1,42 @@
-import React, { useState, createContext  } from "react";
-import { BrowserRouter } from "react-router-dom";
-import { observer } from "mobx-react-lite";
+import React, { useState, createContext } from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { observer } from 'mobx-react-lite'
 
-import { Layout } from "./Layout";
-import Router from "./Royter";
+import { Layout } from './Layout'
+import Router from './Royter'
 
-export const Store = createContext();
-export const ModalWindow = createContext();
+export const Store = createContext()
+export const Modal = createContext()
 
 function App() {
-  const [store, setStore] = useState({
-    user: [{
-      email: "Developments@gmail.com",
-      password: "Devel2022",
-    },],
-    isLoggedIn: false,
-  });
-  const [modal, setModal] = useState({
-    isRegistration:false,
-    isSignIn:false
-  });
-  return (
-    <React.StrictMode>
-      <BrowserRouter>
-        <Store.Provider value={[store, setStore]}>
-          <ModalWindow.Provider value={[modal, setModal]}>
-            <Layout>
-              <Router />
-            </Layout>
-          </ModalWindow.Provider>
-        </Store.Provider>
-      </BrowserRouter>
-    </React.StrictMode>
-  );
+	const [store, setStore] = useState({
+		user: [
+			{
+				email: 'Developments@gmail.com',
+				password: 'Devel2022'
+			}
+		],
+		isLoggedIn: false
+	})
+
+	const [modal, setModal] = useState({
+		isRegistration: false,
+		isSignIn: false
+	})
+
+	return (
+		<React.StrictMode>
+			<BrowserRouter>
+				<Store.Provider value={[store, setStore]}>
+					<Modal.Provider value={[modal, setModal]}>
+						<Layout>
+							<Router />
+						</Layout>
+					</Modal.Provider>
+				</Store.Provider>
+			</BrowserRouter>
+		</React.StrictMode>
+	)
 }
 
-export default observer(App);
+export default observer(App)
--- src/app/Layout/ui/SignIn/SignIn.jsx
+++ src/app/Layout/ui/SignIn/SignIn.jsx
@@ -1,87 +1,127 @@
-import React, { useState } from 'react';
-import { useContext } from 'react' 
+import React, { useState } from 'react'
+import { useContext } from 'react'
 import { observer } from 'mobx-react-lite'
 
-import { Store, ModalWindow } from 'app/App'
-import logoApple from 'assets/img/Registration/Apple.svg';
-import logoFacebook from 'assets/img/Registration/Facebook.svg';
-import logoGoogle from 'assets/img/Registration/Google.svg';
-import logoVk from 'assets/img/Registration/Vk-fill.svg';
-import logoYandex from 'assets/img/Registration/Yandex.svg';
-import Cross from 'assets/img/Registration/Vector.svg';
+import { Store, Modal } from 'app/App'
+import logoApple from 'assets/img/Registration/Apple.svg'
+import logoFacebook from 'assets/img/Registration/Facebook.svg'
+import logoGoogle from 'assets/img/Registration/Google.svg'
+import logoVk from 'assets/img/Registration/Vk-fill.svg'
+import logoYandex from 'assets/img/Registration/Yandex.svg'
+import Cross from 'assets/img/Registration/Vector.svg'
 
-import styles from './styles.module.css';
+import styles from './styles.module.css'
 
-function SignIn(){
-    const [store] = useContext(Store);
-    const [modal] = useContext(ModalWindow);
-    const [register, setRegister] = useState(() => {
-        return {
-            Email: "",
-            Password: "",
-            PasswordRepeat: "",
-        }
-    }) 
+function SignIn() {
+	const [store] = useContext(Store)
+	const [modal, setModal] = useContext(Modal)
+	const [register, setRegister] = useState({
+		Email: '',
+		Password: '',
+		PasswordRepeat: ''
+	})
 
-    function changeInputRegister  (event) {
-        setRegister((prev) => {
-            return {
-                ...prev,
-                [event.target.name]: event.target.value,
-            }
-        })
-    }
-    function submitChackin(){
-        store.user.forEach((element) => {
-            if(element.email == register.Email && element.password == register.Password){
-                alert('True');
-            }else{alert('false')}
-        });
-    }
-    function Entry(name,placeholder){
-        console.log(register,store.user)
-        return (<div className={styles.nameInputWrapper}><p className={styles.nameInput}>{name}*: </p><input 
-        type={String(name)}
-        placeholder={placeholder}
-        id={String(name)}
-        name={String(name)}
-        value={register[String(name)]}
-        onChange={(event)=>changeInputRegister(event)}
-        className = {styles.Input}/></div>
-        )
-    };
+	function changeInputRegister(event) {
+		setRegister((prev) => {
+			return {
+				...prev,
+				[event.target.name]: event.target.value
+			}
+		})
+	}
 
-    function OpenRegister(){
-        modal.isRegistration=!modal.isRegistration;
-        modal.isSignIn=!modal.isSignIn;
-    };
-    if(modal.isSignIn){
-        return(
-            <div className={styles.blur}>
-                <div className={styles.wrapper}>
-                    <div>
-                        <div className={styles.crossWrapeer} onClick={()=>modal.isSignIn=!modal.isSignIn}><img src={Cross} className={styles.cross}/></div>
-                    </div>
-                    <h2 className={styles.title}>Войти:</h2>
-                    <div className={styles.wrapperImg}>
-                        <div className={styles.img}><img src= {logoApple} className={styles.logo} /></div>
-                        <div className={styles.img}><img src= {logoFacebook} className={styles.logo}/></div>
-                        <div className={styles.img}><img src= {logoGoogle} className={styles.logo}/></div>
-                        <div className={styles.img}><img src= {logoVk} className={styles.logo}/></div>
-                        <div className={styles.img}><img src= {logoYandex} className={styles.logo}/></div>
-                    </div>
-                    <h1 className={styles.descriptionIcons}>или зарегистрируйтесь с помощью email</h1>
-                    <form  className={styles.form} onSubmit={submitChackin}>
-                        {Entry('Email', '  Введите Ваш адрес эл. почты')}
-                        {Entry('Password', '  Введите Ваш пароль')}
-                        <input type="submit" value='Войти' className={styles.btn}/>
-                    </form>
-                    <hr className={styles.line}></hr>
-                    <div className={styles.otherInput}><p>Еще не зарегистрированы?</p><p className={styles.textInput} onClick={OpenRegister}>Зарегистрироваться</p></div>
-                    <div className={styles.footer}>Создавая аккаунт я соглашаюсь <p className={styles.footer_title}>c  Политикой конфиденциальности и Условиями пользования</p></div>
-            </div>
-        </div>
-        )
-    }
+	function Entry(name, placeholder) {
+		return (
+			<div className={styles.nameInputWrapper}>
+				<p className={styles.nameInput}>{name}*: </p>
+				<input
+					type={String(name)}
+					placeholder={placeholder}
+					id={String(name)}
+					name={String(name)}
+					value={register[String(name)]}
+					onChange={(event) => changeInputRegister(event)}
+					className={styles.Input}
+				/>
+			</div>
+		)
+	}
+
+	function submitChackin() {
+		store.user.forEach((element) => {
+			if (
+				element.email == register.Email &&
+				element.password == register.Password
+			) {
+				alert('True')
+			} else {
+				alert('false')
+			}
+		})
+	}
+
+	function showSignUpModal() {
+		setModal((pre) => ({
+			...pre,
+			isRegistration: !pre.isRegistration,
+			isSignIn: !pre.isSignIn
+		}))
+	}
+
+	if (modal.isSignIn) {
+		return (
+			<div className={styles.blur}>
+				<div className={styles.wrapper}>
+					<div>
+						<div
+							className={styles.crossWrapeer}
+							onClick={() => setModal((pre) => ({ ...pre, isSignIn: false }))}
+						>
+							<img src={Cross} className={styles.cross} />
+						</div>
+					</div>
+					<h2 className={styles.title}>Войти:</h2>
+					<div className={styles.wrapperImg}>
+						<div className={styles.img}>
+							<img src={logoApple} className={styles.logo} />
+						</div>
+						<div className={styles.img}>
+							<img src={logoFacebook} className={styles.logo} />
+						</div>
+						<div className={styles.img}>
+							<img src={logoGoogle} className={styles.logo} />
+						</div>
+						<div className={styles.img}>
+							<img src={logoVk} className={styles.logo} />
+						</div>
+						<div className={styles.img}>
+							<img src={logoYandex} className={styles.logo} />
+						</div>
+					</div>
+					<h1 className={styles.descriptionIcons}>
+						или зарегистрируйтесь с помощью email
+					</h1>
+					<form className={styles.form} onSubmit={submitChackin}>
+						{Entry('Email', '  Введите Ваш адрес эл. почты')}
+						{Entry('Password', '  Введите Ваш пароль')}
+						<input type='submit' value='Войти' className={styles.btn} />
+					</form>
+					<hr className={styles.line}></hr>
+					<div className={styles.otherInput}>
+						<p>Еще не зарегистрированы?</p>
+						<p className={styles.textInput} onClick={showSignUpModal}>
+							Зарегистрироваться
+						</p>
+					</div>
+					<div className={styles.footer}>
+						Создавая аккаунт я соглашаюсь{' '}
+						<p className={styles.footer_title}>
+							c Политикой конфиденциальности и Условиями пользования
+						</p>
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
-export default observer(SignIn);
+export default observer(SignIn)
