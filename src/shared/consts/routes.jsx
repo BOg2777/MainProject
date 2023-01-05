4c03d7ab28dc2a6a2b344845ca191c8e5c5e941a
Polina Fresher <polina_3@icloud.com>
2023-01-05 20:31:09 +0300

    fix-path

--- src/shared/consts/routes.jsx
+++ src/shared/consts/routes.jsx
@@ -2,7 +2,7 @@ export const ROUTES = {
 	HOME: '/home',
 	NEWS: '/News',
 	NEWS_ID: '/News/:id',
-	TSHIRTS: '/catalog/badminton/mens_clothing/T-shirts',
+	TSHIRTS: '/catalog/badminton/men\'s_clothing/T-shirts',
 	CLUB_PROGRAM: '/clubProgram',
 	FAQ: '/faq',
 	CONTACTS: '/contacts',
